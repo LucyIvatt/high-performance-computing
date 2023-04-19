f9cde9b390423a853eac9622fc650f30d1aabf41
Lucy Ivatt <lucyivatt@gmail.com>
2023-04-19 16:36:55 +0100

    finally got scatterv doing what i want

--- tests/reduction_p0.c
+++ tests/reduction_p0.c
@@ -9,7 +9,7 @@
 int ROOT = 0;
 
 int imax = 5;
-int jmax = 7;
+int jmax = 10;
 
 char **flag;
 double **p;
@@ -63,15 +63,15 @@ int main(int argc, char **argv)
     int ROWS_PER_PROCESS = (arr_size_x - 2) / (process_num - 1);
     int ROW_REMAINDER = (arr_size_x - 2) % ROWS_PER_PROCESS;
 
+    flag = alloc_2d_char_array(arr_size_x, arr_size_y);
+    p = alloc_2d_array(arr_size_x, arr_size_y);
+
     if (rank == ROOT)
     {
         printf("Rows to compute %d\n", arr_size_x - 2);
         printf("Number of processess including host %d\n", process_num);
         printf("Num row per process %d\n", ROWS_PER_PROCESS);
         printf("remainder %d\n", ROW_REMAINDER);
-
-        flag = alloc_2d_char_array(arr_size_x, arr_size_y);
-        p = alloc_2d_array(arr_size_x, arr_size_y);
 
         for (int i = 0; i < imax + 2; i++)
         {
@@ -104,47 +104,47 @@ int main(int argc, char **argv)
         }
     }
 
-    int* send_counts = malloc(process_num * sizeof(int));
-    int* displ = malloc(process_num * sizeof(int));
+    int counts[process_num];
+    int displs[process_num];
 
-    send_counts[0] = ROW_REMAINDER * arr_size_y; // Set first value to remainder elements
-    displ[0] = 0;
-    for (int i = 1; i < process_num; i++) {
-        send_counts[i] = ROWS_PER_PROCESS * arr_size_y; // Set all other values to 1
-        displ[i] = ROW_REMAINDER + ((i-1) * ROWS_PER_PROCESS * arr_size_y);
+    counts[0] = ROW_REMAINDER * arr_size_y;
+    displs[0] = arr_size_y;
+
+    for (int i = 1; i < process_num; i++)
+    {
+        counts[i] = ROWS_PER_PROCESS * arr_size_y;
+        displs[i] = arr_size_y + (ROW_REMAINDER * arr_size_y) + (ROWS_PER_PROCESS * arr_size_y * (i - 1));
     }
 
-    // Storage for p0 on each rank
-    double *p_rows = malloc(sizeof(double) * send_counts[rank]);
-    char *flag_rows = malloc(sizeof(double) * send_counts[rank]);
+    if (rank == 0)
+    {
 
-    double local_p0 = 0;
+        for (int i = 0; i < process_num; i++)
+        {
+            printf("counts[%d]=%d\n", i, counts[i]);
+        }
 
-    MPI_Barrier(MPI_COMM_WORLD);
+        printf("\n");
 
-    MPI_Scatterv(&(p[0]), send_counts, displ, MPI_DOUBLE, p_rows, send_counts[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatterv(&(flag[0]), send_counts, displ, MPI_CHAR, flag_rows, send_counts[rank], MPI_CHAR, 0, MPI_COMM_WORLD);
-
-    int num_rows = (rank == 0) ? ROW_REMAINDER : ROWS_PER_PROCESS;
-
-    for (int i = 0; i < num_rows; i++) {
-        for (int j = 1; j < arr_size_y-1; j++){
-            if (flag_rows[test_ind(i, j)] & C_F)
-                {
-                    local_p0 += p_rows[test_ind(i, j)] * p_rows[test_ind(i, j)];
-                }
+        for (int i = 0; i < process_num; i++)
+        {
+            printf("displs[%d]=%d\n", i, displs[i]);
         }
     }
 
-    printf("p0 on rank %d = %f\n", rank, local_p0);
+    double *local_A = (double *)malloc(counts[rank] * sizeof(double));
 
-    // Reduce all of the local sums into the global sum
-    double p0;
-    MPI_Reduce(&local_p0, &p0, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Scatter the data to each process
+    MPI_Scatterv(p[0], counts, displs, MPI_DOUBLE, local_A, counts[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    // Print the result
-    if (rank == 0) {
-    printf("\nglobal p0 = %f", p0);
+    // Each process now has a local buffer with only a subset of the rows
+    if (rank == 2)
+    {
+        printf("Process 0 received:\n");
+        for (int i = 0; i < counts[2]; i++)
+        {
+            printf("%.0f ", local_A[i]);
+        }
     }
 
     MPI_Finalize();
