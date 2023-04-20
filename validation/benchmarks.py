21789635f7a590a83e05054295a81f4601cad5d3
Lucy Ivatt <lucyivatt@gmail.com>
2023-04-20 19:09:09 +0100

    renamed benchmark file, added confirmations, changed upper and lower bounds, added printing before running

---
+++ validation/benchmarks.py
@@ -0,0 +1,114 @@
+import os
+import sys
+import subprocess
+import re
+
+START = 100
+STEP = 100
+END_SIZE = 2000
+
+if sys.argv[1] == "original":
+    if input("Are you sure you want to run all original benchmarks on Viking? ") != "yes":
+        exit()
+
+    prefix = "just viking_run original "
+    for x in range(START, END_SIZE+STEP, STEP):
+        y = int(x / 4)
+        folder_name = str(f"x_{x}_y_{y}_orig")
+        cmd = prefix + folder_name + " original_benchmarks" + f" -x {x} -y {y}"
+
+        if sys.argv[2] == "print":
+            print(cmd)
+        else:
+            os.system(cmd)
+
+elif sys.argv[1] == "openmp" and sys.argv[2] == "cpus":
+    if input("Are you sure you want to run openmp cpu experiment on Viking? ") != "yes":
+        exit()
+
+    prefix = "just viking_run_openmp "
+    for cpus in range(5, 45, 5):
+        folder_name = str(f"{cpus}_cpus_omp ")
+        cmd = prefix + folder_name + str(cpus) + " openmp_cpu_experiment"
+        
+        if sys.argv[3] == "print":
+            print(cmd)
+        else:
+            os.system(cmd)
+    
+elif sys.argv[1] == "openmp" and sys.argv[2] == "benchmarks":
+    if input("Are you sure you want to run openmp benchmarks on Viking? ") != "yes":
+        exit()
+
+    prefix = "just viking_run_openmp "
+    for x in range(START, END_SIZE+STEP, STEP):
+        y = int(x / 4)
+        folder_name = str(f"x_{x}_y_{y}_omp")
+        cmd = prefix + folder_name + " 20 openmp_benchmarks" + f" -x {x} -y {y}"
+        if sys.argv[3] == "print":
+            print(cmd)
+        else:
+            os.system(cmd)
+
+elif sys.argv[1] == "cuda" and sys.argv[2] == "checkpoints":
+    if input("Are you sure you want to run cuda checkpoint experiment on Viking? ") != "yes":
+        exit()
+
+    prefix = "just viking_run_cuda "
+    for x in range(START, END_SIZE+STEP, STEP):
+        y = int(x / 4)
+        folder_name = str(f"x_{x}_y_{y}_cuda_cp")
+        cmd = prefix + folder_name + " cuda_checkpoint_experiment" + f" -x {x} -y {y} -c"
+
+        if sys.argv[3] == "print":
+            print(cmd)
+        else:
+            os.system(cmd)
+
+
+elif sys.argv[1] == "cuda" and sys.argv[2] == "benchmarks":
+    if input("Are you sure you want to run cuda benchmarks on Viking? ") != "yes":
+        exit()
+
+    prefix = "just viking_run_cuda "
+    for x in range(START, END_SIZE+STEP, STEP):
+        y = int(x / 4)
+        folder_name = str(f"x_{x}_y_{y}_cuda")
+        cmd = prefix + folder_name + " cuda_benchmarks" + f" -x {x} -y {y}"
+        
+        if sys.argv[3] == "print":
+            print(cmd)
+        else:
+            os.system(cmd)
+
+elif sys.argv[1] == "mpi":
+    pass
+
+elif sys.argv[1] == "slurm_copy" and sys.argv[2] in ["original_benchmarks", "openmp_benchmarks", "openmp_cpu_experiment", "cuda_checkpoint_experiment", "cuda_benchmarks"]:
+    path = "/mnt/c/Users/lucea/Documents/Github Repos/HIPC-Assessment/validation/" + sys.argv[2]
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+    if not os.path.isfile(path + "/" + sys.argv[2] + "_data.csv"):
+        with open(path + "/" + sys.argv[2] + "_data.csv", "w") as f:
+            for line in sys.stdin:
+                line = line.rstrip("\n")
+                split_path = line.split("/")
+                os.system(f"just viking_rsync_from '~/scratch/{sys.argv[2]}/{split_path[-2]}/{split_path[-1]}' '{path}/{split_path[-2]}.out'")
+
+                with open(f"{path}/{split_path[-2]}.out", "r") as slurm_log:
+                    for log_line in slurm_log:
+                        match = re.search(r'Total Time:\s+(\d+\.\d+)', log_line)
+                        if match:
+                            f.write(f"{split_path[-2][:-4]}, {match.group(1)}\n")
+                            break
+                    else:
+                        f.write(f"{split_path[-2][:-5]}, {600}\n")
+
+    else:
+        print("data.csv already exists, clear directory and retry.")
+else:
+    print("Incorrect script arguments, please try again.")
+
+
