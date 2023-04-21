bb92d590659286b35bb1a3ff65484377a479e44f
Lucy Ivatt <lucyivatt@gmail.com>
2023-04-21 17:51:46 +0100

    broadcast updated p0 to all processes

--- mpi/vortex.c
+++ mpi/vortex.c
@@ -173,9 +173,9 @@ void compute_rhs(int rank, int process_num)
                 if (flag_buff[ind_ret(i, j)] & C_F)
                 {
                     /* only for fluid and non-surface cells */
-                    rhs_buff_send[ind(i, j)] = ((f_buff[ind_ret(i, j)] - f_buff[ind_ret(i-1, j)]) / delx +
-                                 (g_buff[ind_ret(i, j)] - g_buff[ind_ret(i, j-1)]) / dely) /
-                                del_t;
+                    rhs_buff_send[ind(i, j)] = ((f_buff[ind_ret(i, j)] - f_buff[ind_ret(i - 1, j)]) / delx +
+                                                (g_buff[ind_ret(i, j)] - g_buff[ind_ret(i, j - 1)]) / dely) /
+                                               del_t;
                 }
             }
             // Sets values at the end of the rows to what they already were
@@ -204,7 +204,7 @@ double poisson(int rank, int process_num)
     int displs[process_num]; // where to access the elements from the arrays
 
     counts[0] = ROW_REMAINDER * arr_size_y; // root node only processes remainder rows
-    displs[0] = arr_size_y; // automatically displaced by a row as we only care about inner values for p0
+    displs[0] = arr_size_y;                 // automatically displaced by a row as we only care about inner values for p0
 
     for (int i = 1; i < process_num; i++)
     {
@@ -218,23 +218,25 @@ double poisson(int rank, int process_num)
     double local_p0 = 0;
 
     // Sends the relevant part of the p and flag arrays to the processes
-    MPI_Scatterv(p[0], counts, displs, MPI_DOUBLE, p_rows, counts[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    MPI_Scatterv(flag[0], counts, displs, MPI_CHAR, flag_rows, counts[rank], MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(p[0], counts, displs, MPI_DOUBLE, p_rows, counts[rank], MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    MPI_Scatterv(flag[0], counts, displs, MPI_CHAR, flag_rows, counts[rank], MPI_CHAR, ROOT, MPI_COMM_WORLD);
 
     int num_rows = (rank == 0) ? ROW_REMAINDER : ROWS_PER_PROCESS;
 
-    for (int i = 0; i < num_rows; i++) {
-        for (int j = 1; j < arr_size_y-1; j++){
+    for (int i = 0; i < num_rows; i++)
+    {
+        for (int j = 1; j < arr_size_y - 1; j++)
+        {
             if (flag_rows[ind(i, j)] & C_F)
-                {
-                    local_p0 += p_rows[ind(i, j)] * p_rows[ind(i, j)];
-                }
+            {
+                local_p0 += p_rows[ind(i, j)] * p_rows[ind(i, j)];
+            }
         }
     }
 
     double p0;
     MPI_Reduce(&local_p0, &p0, 1, MPI_DOUBLE, MPI_SUM, 0,
-           MPI_COMM_WORLD);
+               MPI_COMM_WORLD);
 
     if (rank == ROOT)
     {
@@ -243,78 +245,81 @@ double poisson(int rank, int process_num)
         {
             p0 = 1.0;
         }
+        MPI_Bcast(&p0, 1, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    }
 
-        /* Red/Black SOR-iteration */
-        int iter;
-        double res = 0.0;
-        for (iter = 0; iter < itermax; iter++)
+
+
+    /* Red/Black SOR-iteration */
+    int iter;
+    double res = 0.0;
+    for (iter = 0; iter < itermax; iter++)
+    {
+        for (int rb = 0; rb < 2; rb++)
         {
-            for (int rb = 0; rb < 2; rb++)
-            {
-                for (int i = 1; i < imax + 1; i++)
-                {
-                    for (int j = 1; j < jmax + 1; j++)
-                    {
-                        if ((i + j) % 2 != rb)
-                        {
-                            continue;
-                        }
-                        if (flag[i][j] == (C_F | B_NSEW))
-                        {
-                            /* five point star for interior fluid cells */
-                            p[i][j] = (1.0 - omega) * p[i][j] -
-                                      beta_2 * ((p[i + 1][j] + p[i - 1][j]) * rdx2 + (p[i][j + 1] + p[i][j - 1]) * rdy2 - rhs[i][j]);
-                        }
-                        else if (flag[i][j] & C_F)
-                        {
-                            /* modified star near boundary */
-
-                            double eps_E = ((flag[i + 1][j] & C_F) ? 1.0 : 0.0);
-                            double eps_W = ((flag[i - 1][j] & C_F) ? 1.0 : 0.0);
-                            double eps_N = ((flag[i][j + 1] & C_F) ? 1.0 : 0.0);
-                            double eps_S = ((flag[i][j - 1] & C_F) ? 1.0 : 0.0);
-
-                            double beta_mod = -omega / ((eps_E + eps_W) * rdx2 + (eps_N + eps_S) * rdy2);
-                            p[i][j] = (1.0 - omega) * p[i][j] -
-                                      beta_mod * ((eps_E * p[i + 1][j] + eps_W * p[i - 1][j]) * rdx2 + (eps_N * p[i][j + 1] + eps_S * p[i][j - 1]) * rdy2 - rhs[i][j]);
-                        }
-                    }
-                }
-            }
-
-            /* computation of residual */
             for (int i = 1; i < imax + 1; i++)
             {
                 for (int j = 1; j < jmax + 1; j++)
                 {
-                    if (flag[i][j] & C_F)
+                    if ((i + j) % 2 != rb)
                     {
+                        continue;
+                    }
+                    if (flag[i][j] == (C_F | B_NSEW))
+                    {
+                        /* five point star for interior fluid cells */
+                        p[i][j] = (1.0 - omega) * p[i][j] -
+                                  beta_2 * ((p[i + 1][j] + p[i - 1][j]) * rdx2 + (p[i][j + 1] + p[i][j - 1]) * rdy2 - rhs[i][j]);
+                    }
+                    else if (flag[i][j] & C_F)
+                    {
+                        /* modified star near boundary */
+
                         double eps_E = ((flag[i + 1][j] & C_F) ? 1.0 : 0.0);
                         double eps_W = ((flag[i - 1][j] & C_F) ? 1.0 : 0.0);
                         double eps_N = ((flag[i][j + 1] & C_F) ? 1.0 : 0.0);
                         double eps_S = ((flag[i][j - 1] & C_F) ? 1.0 : 0.0);
 
-                        /* only fluid cells */
-                        double add = (eps_E * (p[i + 1][j] - p[i][j]) -
-                                      eps_W * (p[i][j] - p[i - 1][j])) *
-                                         rdx2 +
-                                     (eps_N * (p[i][j + 1] - p[i][j]) -
-                                      eps_S * (p[i][j] - p[i][j - 1])) *
-                                         rdy2 -
-                                     rhs[i][j];
-                        res += add * add;
+                        double beta_mod = -omega / ((eps_E + eps_W) * rdx2 + (eps_N + eps_S) * rdy2);
+                        p[i][j] = (1.0 - omega) * p[i][j] -
+                                  beta_mod * ((eps_E * p[i + 1][j] + eps_W * p[i - 1][j]) * rdx2 + (eps_N * p[i][j + 1] + eps_S * p[i][j - 1]) * rdy2 - rhs[i][j]);
                     }
                 }
             }
-            res = sqrt(res / fluid_cells) / p0;
-
-            /* convergence? */
-            if (res < eps)
-                break;
         }
 
-        return res;
+        /* computation of residual */
+        for (int i = 1; i < imax + 1; i++)
+        {
+            for (int j = 1; j < jmax + 1; j++)
+            {
+                if (flag[i][j] & C_F)
+                {
+                    double eps_E = ((flag[i + 1][j] & C_F) ? 1.0 : 0.0);
+                    double eps_W = ((flag[i - 1][j] & C_F) ? 1.0 : 0.0);
+                    double eps_N = ((flag[i][j + 1] & C_F) ? 1.0 : 0.0);
+                    double eps_S = ((flag[i][j - 1] & C_F) ? 1.0 : 0.0);
+
+                    /* only fluid cells */
+                    double add = (eps_E * (p[i + 1][j] - p[i][j]) -
+                                  eps_W * (p[i][j] - p[i - 1][j])) *
+                                     rdx2 +
+                                 (eps_N * (p[i][j + 1] - p[i][j]) -
+                                  eps_S * (p[i][j] - p[i][j - 1])) *
+                                     rdy2 -
+                                 rhs[i][j];
+                    res += add * add;
+                }
+            }
+        }
+        res = sqrt(res / fluid_cells) / p0;
+
+        /* convergence? */
+        if (res < eps)
+            break;
     }
+
+    return res;
 }
 
 /**
